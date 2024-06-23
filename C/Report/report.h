28d456d33e7dae09105e12e592545d29cd091e5f
val1975-Pinsk <bvl2004@mail.ru>
2024-06-23 05:19:20 +0000

    переключился на gedit

--- C/Report/report.h
+++ C/Report/report.h
@@ -1,11 +1,11 @@
-#define     OpenTag                 60      //  Открывающий тег
-#define     CloseTag                  62       //  Закрывающий тег
+#define     OpenTag     60      //  Открывающий тег
+#define     CloseTag    62      //  Закрывающий тег
 
-#define     yes                           1                   //  да - 1
-#define     no                            0                    //  нет - 0
+#define     yes         1       //  да - 1
+#define     no          0       //  нет - 0
 
-#define     ERR                         "ошибка\n"
-#define     SUCS                       "успешно\n"
+#define     ERR         "ошибка\n"
+#define     SUCS        "успешно\n"
 
 struct message {
         char * err;
@@ -15,8 +15,8 @@ struct message {
 extern int getMonthDigit () ;
 extern int getMonthDigit_2 () ;
 /*
-                Функция проверяет является ли символ числом.
-        Функция возвращает 1 - если символ число, иначе возвращается 0.
+        Функция проверяет является ли символ числом.
+Функция возвращает 1 - если символ число, иначе возвращается 0.
 */
 extern int isDigit ( int c ) ;
 /**/
