ce47514251e2cc679220d5e28469b803ccb5a2f3
val1975-Pinsk <bvl2004@mail.ru>
2024-07-03 18:48:04 +0000

    разбил строку на дату, время и маршрут

--- C/Report/reportTest.c
+++ C/Report/reportTest.c
@@ -10,18 +10,74 @@ char * p_b = buffer;
 void main ( void ) {
         printf ( "Открытие файла:\n");
         printf ( "%s\n", fileName ) ;
-        FILE * p_f = fopen ( fileName, "r" );
+        FILE * p_f = fopen ( fileName, "r" );                                   //Открываем файл.
         if ( p_f == NULL ) {
                 printf ( "%s", messg.err );
         } else {
                 printf ( "%s", messg.sucs );
-                //      Чтение строки из файла
-                //char * str = "			<td colspan=\"5\">2024-06-16, 17:45, Пинск-Минск</td>";
-                char * pattern = "colspan=\"5\"";
+                char * colspan_5 = "colspan=\"5\"";
                 while ((fgets(buffer, 256, p_f)) != NULL){
-                        if(strInStr(p_b, pattern)){
-                                printf("%s\n", p_b);
-                        }//else printf("Pattern not in string!\n");
+                        if(strInStr(p_b, colspan_5)){
+                                char * pattern;
+                                if(strInStr(p_b, pattern = "Пинск")){           //Здесь ищем строку с названием маршрута.
+                                        //Перемещаем указатель к началу содержимого строки/
+                                        while(1){
+                                                if(*p_b == CloseTag){
+                                                        *(p_b++);        
+                                                        break;
+                                                }
+                                                *(p_b++);
+                                        }
+                                        printf("Дата: \t\t   ");
+                                        while(1){
+                                                if(*p_b == ','){
+                                                        *(p_b += 2);
+                                                        break;
+                                                }
+                                                printf("%c", *p_b);
+                                                *(p_b++);
+                                        }
+                                        printf("\nВремя отправления: ");
+                                        while(1){
+                                                if(*p_b == ','){
+                                                        *(p_b += 2);
+                                                        break;
+                                                }
+                                                printf("%c", *p_b);
+                                                *(p_b++);
+                                        }
+                                        printf("\nМаршрут: \t   ");
+                                        while(1){
+                                                if(*p_b == OpenTag){
+                                                        *(p_b += 2);
+                                                        printf("\n");
+                                                        break;
+                                                }
+                                                printf("%c", *p_b);
+                                                *(p_b++);
+                                        }
+                                        //Берём из строки нужное содержимое.
+                                        /*int getContent = 0;
+                                        char str[256];
+                                        char * p_str = str;
+                                        printf("before While: getContent = %d\n", getContent);
+                                        while(1){
+                                                if((*p_b == CloseTag)&&(getContent == 0)){
+                                                        getContent = 1;
+                                                        *(p_b++);
+                                                        printf("after CloseTag: getContent = %d\n", getContent);
+                                                }else if((*p_b == OpenTag)&&(getContent == 1)){
+                                                        printf("\n");
+                                                        getContent = 0;
+                                                        break;
+                                                }
+                                                if(getContent) printf("%c", *p_b);
+                                        }*/
+                                }
+                                if(strInStr(p_b, pattern = "свободно")){   //Здесь ищем строку с названием автомобиля.
+                                        printf("%s", p_b);
+                                }
+                        }
                                                 
                 }
         }
