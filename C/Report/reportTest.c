a9096748abf963352d1d086999ead22a27559c17
val1975-Pinsk <bvl2004@mail.ru>
2024-07-10 06:26:04 +0000

    Реализовал подсчет количества пассажиров по оплате.

--- C/Report/reportTest.c
+++ C/Report/reportTest.c
@@ -12,6 +12,29 @@ struct reportPass {
 char * fileName = "Водители.html";
 char buffer [ 256 ];
 char * p_b;
+char * hlf = "17р";
+char * dsc = "Д.К.";
+char * dsc1 = "Дк";
+char * dsc2 = "д.к.";
+
+int strInStrCount(char * str, char * pattern){  //Ln 97
+        //printf("from strInStrCount: %s\n", str);
+        char * ptn = pattern;
+        int count = 0;
+        while(*str != '\0'){
+                if(*str == *ptn){
+                        *(ptn++);
+                        if(*ptn == '\0'){
+                                count++;
+                                ptn = pattern;
+                        }
+                }else ptn = pattern;
+                *(str++);
+        }
+        //printf("pattern: %s\n", pattern);
+        //printf("count: %d\n", count);
+        return count;
+}
 
 void main ( void ) {
         printf ( "Открытие файла:\n");
@@ -25,10 +48,11 @@ void main ( void ) {
                 char * colspan_5 = "colspan=\"5\"";
                 char * selected = "selected=\"selected\"";
                 char * width_25 = "width=\"25px\"";
+                char * endOfReport = "tr height=\"40px\"";
                 //char * pattern;
                 int go = no;
                 int count = 0;
-                int digit;
+                int total;
                 while ((fgets(buffer, 256, p_f)) != NULL){
                         p_b = buffer;
                         //go = no;
@@ -47,16 +71,17 @@ void main ( void ) {
                                 p_b = movePointerToChar(p_b, CloseTag, 0);
                                 printf("\nЗанято: \t   ");
                                 //конвертируем количество занятых мест в числовое выражение.
-                                digit = charToDigit(p_b);
-                                if(digit != -1){
-                                        count = digit;
+                                total = charToDigit(p_b);
+                                if(total != -1){
+                                        count = total;
                                         *(p_b++);
                                 }
-                                digit = charToDigit(p_b);
-                                if(digit != -1){
-                                        count = count * 10 + digit;
+                                total = charToDigit(p_b);
+                                if(total != -1){
+                                        count = count * 10 + total;
                                         *(p_b++);
                                 }
+                                total = count;
                                 p_b = movePointerToChar(p_b, ',', 1);
                                 printf(",");
                                 p_b = movePointerToChar(p_b, ',', 1);
@@ -64,18 +89,55 @@ void main ( void ) {
                                 p_b = movePointerToChar(p_b, OpenTag, 1);
                                 printf("\n");
                                 printf("__________________________________________________\n");
-//                                printf("Поехало %d\n", count);
+                                
                         }
                         if(strInStr(p_b, selected) && strInStr(p_b, "Поехал")){
-                                printf("go!\n");
+                                //printf("go!\n");
                                 go = yes;
                         }
-                        /*if(strInStr(p_b, "дк") && go == yes){
-                                printf("%s", p_b);
-                        }*/
                         if(strInStr(p_b, colspan_5) && strInStr(p_b, "+")){
-                                p_b = movePointToChar(p_b, CloseTag);
-                                
+                                //int count;
+                                p_b = buffer;
+                                count = strInStrCount(p_b, "Д.К.");   //Ln 20
+                                if(count != 0 && go == yes){
+                                        rP.discount += count;
+                                }
+                                count = strInStrCount(p_b, "Дк");
+                                if(count != 0 && go == yes){
+                                        rP.discount += count;
+                                }
+                                count = strInStrCount(p_b, "д.к.");
+                                if(count != 0 && go == yes){
+                                        rP.discount += count;
+                                }
+                                count = strInStrCount(p_b, "17р");
+                                if(count != 0 && go == yes){
+                                        rP.half += count;
+                                }
+                                count = strInStrCount(p_b, "дк");
+                                if(count != 0 && go == yes){
+                                        rP.discount += count;
+                                }
+                                count = strInStrCount(p_b, "бесплатно");
+                                if(count != 0 && go == yes){
+                                        rP.noCash += count;
+                                }
+                                count = strInStrCount(p_b, "б/н");
+                                if(count != 0 && go == yes){
+                                        rP.noCash += count;
+                                }
+                                go = no;
+                        }
+                        if(strInStr(p_b, endOfReport)){
+                                printf("17р %d человек\n", rP.half);
+                                printf("30р %d человек\n", rP.discount);
+                                rP.full = total - (rP.half + rP.discount + rP.noCash);
+                                printf("35р %d человек\n", rP.full);
+                                printf("б/н или без оплаты %d человек\n", rP.noCash);
+                                rP.full = 0;
+                                rP.discount = 0;
+                                rP.noCash = 0;
+                                rP.half = 0;
                         }
                 }
         }
