ba9fe241414db0dc8cc7679c652165c463468179
val1975-Pinsk <113911208+val1975-Pinsk@users.noreply.github.com>
2024-08-15 08:42:41 +0300

    Update reportLib.c

--- C/Report/reportLib.c
+++ C/Report/reportLib.c
@@ -64,13 +64,55 @@ int strInStr(char * str, char * pattern){
                         //*(ptn++);
                         ptn += 1;
                         if(*ptn == '\0'){
-                                return 1;
+                                return yes;
                         }
                 }else ptn = pattern;
                 //*(str++);
                 str += 1;
         }
-        return 0;
+        return no;
+}
+/**/
+int strIsName(char * srcStr){
+  char * pattern = "colspan=\"3\"";
+  return strInStr(srcStr, pattern);
+}
+/**/
+int strIsPayment(char * srcStr){
+  char * pattern = "colspan=\"5\"";
+  return strInStr(srcStr, pattern);
+}
+/*
+  Функция проверяет была ли произведена оплата по безналичному расчёту.
+ */
+int isNoCash(char * srcStr){
+  if (strInStr(srcStr, "безнал")){
+    return yes;
+  }else if (strInStr(srcStr, "б/н")){
+    return yes;
+  }else if (strInStr(srcStr, "безнл")){
+    return yes;
+  }else if (strInStr(srcStr, "бесплатно")){
+    return yes;
+  }else return strInStr(srcStr, "оплачено");
+}
+/**/
+void clearPassengerName(char * passName, int sizeOfArr){
+  memset(passName, ' ', sizeOfArr);
+}
+/**/
+void writePassName(char * srcPointer, char * targetStr){
+  srcPointer = movePointerToChar(srcPointer, CloseTag, 0);
+  *targetStr = *srcPointer;
+  while(1){
+    targetStr += 1;
+    srcPointer += 1;
+    if(*srcPointer == OpenTag){
+      srcPointer = '\0';
+      break;
+    }
+    *targetStr = *srcPointer;
+  }
 }
 /*
         Функция передвигает указатель до указанного символа chr.
@@ -150,7 +192,6 @@ int strInStrCount(char * str, char * pattern){
 int getDiscountCount(char * srcStr){
         int count = 0;
         count = strInStrCount(srcStr, "Д.К.");
-	count = strInStrCount(srcStr, "ДК");
         count += strInStrCount(srcStr, "Дк");
         count += strInStrCount(srcStr, "д.к."); 
         count += strInStrCount(srcStr, "дк");
@@ -165,42 +206,4 @@ int getNoCashCount(char * srcStr){
         count += strInStr(srcStr, "безнл");
         count += strInStr(srcStr, "оплачено");
         return count;
-}
-/**/
-void printHeaderPart_1(char * p_b){
-  printf("==================================================\n");
-  //Перемещаем указатель к началу содержимого строки/
-  p_b = movePointerToChar(p_b, CloseTag, 0);
-  printf("Дата: \t\t   ");
-  p_b = movePointerToChar(p_b, ',', 1);
-  printf("\nВремя отправления:");
-  p_b = movePointerToChar(p_b, ',', 1);
-  printf("\nМаршрут: \t  ");
-  p_b = movePointerToChar(p_b, OpenTag, 1);
-}
-/**/
-int printHeaderPart_2(char * p_b){
-  int digit, count;
-  p_b = movePointerToChar(p_b, CloseTag, 0);
-  printf("\nЗанято: \t   ");
-  digit = charToDigit(p_b);               //      Конвертируем количество занятых 
-  if(digit != -1){                        //мест в числовое выражение.
-    count = digit;                  //
-  //*(p_b++);                       //warning: value computed is not used
-  }                                       //
-  p_b += 1;
-  digit = charToDigit(p_b);               //
-  if(digit != -1){                        //
-    count = count * 10 + digit;           //
-  }                                       //
-  digit = count;
-  p_b += 1; 
-  p_b = movePointerToChar(p_b, ',', 1);
-  printf(",");
-  p_b = movePointerToChar(p_b, ',', 1);
-  printf("\nАвтомобиль: \t  ");
-  p_b = movePointerToChar(p_b, OpenTag, 1);
-  printf("\n");
-  printf("__________________________________________________\n");
-  return digit;
 }
